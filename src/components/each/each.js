c7267119bcc732a05b5bf0f06eac5450eb97d21e
Patryk Karwowski <patryk@patryks-mbp.home>
2019-12-07 20:17:28 +0000

    Each: using table

--- src/components/each/each.js
+++ src/components/each/each.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 export const Each = () => {
   return (
-    <>
-      <h1>Temp test</h1>
-      <p>text part 1</p>
-      <p> some text</p>
-      <p> another line </p>
-      <p>test footer </p>
-    </>
+    <article>
+      <h1>What is Lorem Ipsum?</h1>
+      <p>Lorem Ipsum is simply dummy text</p>
+      <p>of the printing</p>
+      <p>and typesetting industry</p>
+    </article>
   )
 }
--- src/components/each/each.test.js
+++ src/components/each/each.test.js
@@ -2,23 +2,35 @@ import React from 'react'
 import { mount } from 'enzyme'
 import { Each } from './each'
 
-describe('Temp Test', () => {
+describe('Test each method', () => {
   const component = mount(<Each />)
   const allComponentText = component.text()
 
-  describe('test each as a array', () => {
-    const textPart1 = ['1', 'Temp test']
-    const textPart2 = ['2', 'text part 1']
-    const textPart3 = ['3', 'some text']
-    const textPart4 = ['4', 'another line']
-    const textPart5 = ['5', 'test footer']
-    const allPhrases = [textPart1, textPart2, textPart3, textPart4, textPart5]
+  describe('with an array', () => {
+    const textPart1 = ['1', 'What is Lorem Ipsum?']
+    const textPart2 = ['2', 'Lorem Ipsum is simply dummy text']
+    const textPart3 = ['3', 'of the printing']
+    const textPart4 = ['4', 'and typesetting industry']
 
-    it.each(allPhrases)('renders text part %s', (testNum, text) => {
+    it.each([
+      textPart1, 
+      textPart2, 
+      textPart3, 
+      textPart4
+    ])('match text part %s', (testNum, text) => {
       expect(allComponentText.includes(text)).toBe(true)
     })
   })
 
-  describe('test each as a table', () => {
+  describe('with a table', () => {
+    it.each`
+    testNum | text
+    ${1}    | ${'What is Lorem Ipsum?'}
+    ${2}    | ${'Lorem Ipsum is simply dummy text'}
+    ${3}    | ${'of the printing'}
+    ${4}    | ${'and typesetting industry'}
+    `('match text part $testNum', ({text}) => {
+      expect(allComponentText.includes(text)).toBe(true)
+    })
   })
 })
