18ddb31fbf44a5e0799be063a21c7ca7b173b4ed
Patryk Karwowski <patryk@patryks-mbp.home>
2019-12-08 00:21:18 +0000

    Snapshot: add functional test

--- src/components/snapshot/snapshot.js
+++ src/components/snapshot/snapshot.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
-export const Snapshot = ({isFeatureToggled}) => (
-    <>
-        <h1>Test name: snapshot</h1>
-        {isFeatureToggled
-            ? <p>Feature is switch on</p>
-            : <p>This time feature is switch off</p>
-        }
-    </>
-)
+export const Snapshot = ({isFeatureToggled, link, onClick}) => {
+    return (
+        <>
+            <h1>Test name: snapshot</h1>
+        
+            {isFeatureToggled
+                ? <p>Feature is switch on</p>
+                : <p>This time feature is switch off</p>
+            }
+
+            <button 
+                data-test-id={'snapshot-button'} 
+                href={link || '#'} 
+                onClick={() => {
+                    onClick(link)
+                }} 
+            />
+        </>
+    )
+}
--- src/components/snapshot/snapshot.test.js
+++ src/components/snapshot/snapshot.test.js
@@ -3,13 +3,23 @@ import { Snapshot } from './snapshot'
 
 describe('Snapshot', () => {
     it('Renders component with switch off feature', () => {
-        const component = mount(<Snapshot isFeatureToggled={false}/>)
+        const component = mount(<Snapshot />)
         expect(component).toMatchSnapshot()
     })
 
     it('Renders info when feature is on', () => {
-        const component = mount(<Snapshot isFeatureToggled/>)
+        const component = shallow(<Snapshot isFeatureToggled/>)
         const searchingText = 'Feature is switch on'
         expect(component.text().includes(searchingText)).toBe(true)
+    })
+
+    it('Return link on click', () => {
+        const mockOnClick = jest.fn()
+        const mockLink = 'http://test/link'
+        const component = shallow(<Snapshot link={mockLink} onClick={mockOnClick} />)
+        const button = component.find('[data-test-id="snapshot-button"]')
+        button.simulate('click')
+        expect(mockOnClick).toBeCalledTimes(1)
+        expect(mockOnClick).toBeCalledWith(mockLink)
     })
 })
